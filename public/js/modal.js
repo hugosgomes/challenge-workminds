1338667b2102f418a8cbda716f9a6a4c960ce69f
Hugo Gomes <hdsg0807@gmail.com>
2020-05-03 00:57:36 -0300

    Fix.

--- public/js/modal.js
+++ public/js/modal.js
@@ -28,7 +28,7 @@ function abrirModalEditEstado(index) {
 }
 
 function abrirModalSaveCidade() {
-    if (estadoSelecionado != null) {
+    if (estadoSelecionado === null) {
         alert('Selecione um Estado');
         return;
     }
